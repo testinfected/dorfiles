acae3b9f60c8140725b256159679976f198dd114
Vincent Tencé <vtence@gmail.com>
2023-08-01 11:12:24 -0400

    feat(wezterm): hide tab bar and window decorations

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -10,14 +10,27 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+-- tic -x -o ~/.terminfo ~/.config/wezterm/wezterm.terminfo
+config.term = "wezterm"
+
 -- This is where you actually apply your config choices
 config.font = wezterm.font 'MesloLGS Nerd Font'
 
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
 
--- config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
 
+config.window_background_opacity = 0.90
+config.macos_window_background_blur = 8 
+config.window_padding = {
+  left = '2cell',
+  right = '2cell',
+  top = '1cell',
+  bottom = '1cell',
+}
+
+config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
