14b1f290b66984a399f625239b8632fa19293636
Vincent Tencé <vtence@gmail.com>
2023-08-03 12:30:39 -0400

    feat(neovim): tweak which-key appearance

--- dot_config/nvim/lua/config/which-key.lua
+++ dot_config/nvim/lua/config/which-key.lua
@@ -2,12 +2,18 @@ local wk = require("which-key")
 
 wk.setup {
   window = {
-    border = "solid", -- none, single, double, shadow
+    border = "rounded", -- none, single, double, shadow
     align = 'center',
-    layout = {
-      spacing = 3
-    }
-  }
+    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    winblend = 0,
+  },
+  layout = {
+      height = { min = 4, max = 25 }, -- min and max height of the columns
+      width = { min = 20, max = 50 }, -- min and max width of the columns
+      spacing = 3, -- spacing between columns
+      align = "left", -- align columns left, center or right
+  },
 }
 
 wk.register({
