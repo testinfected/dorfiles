b47c86eb6d21cc6c3acfb36b570cc850b7517687
Vincent Tencé <vtence@gmail.com>
2023-08-10 16:53:36 -0400

    feat(neovim): add colorizer plugin

---
+++ dot_config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,24 @@
+--[[
+  File: colorizer.lua
+  Description: High-performance color highlighter
+  See: https://github.com/NvChad/nvim-colorizer.lua
+]]
+
+local M = {
+    'NvChad/nvim-colorizer.lua',
+    event = 'BufReadPre',
+    keys = {
+        { '<leader>cc', '<Cmd>ColorizerToggle<CR>', "Toggle color highlighting" }
+    }
+}
+
+function M.config()
+    require 'colorizer'.setup {
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    }
+end
+
+return M
