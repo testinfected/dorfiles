daa2e6caa5cac240dd08620c3df5751421d96672
Vincent Tencé <vtence@gmail.com>
2023-08-23 16:24:20 -0400

    feat(nvim): replace lsp code actions with go.nvim
    When a go file is detected, let `go.nvim` process
    code actions.

--- dot_config/nvim/lua/plugins/lang/go.lua
+++ dot_config/nvim/lua/plugins/lang/go.lua
@@ -29,7 +29,17 @@ function M.config()
   require('go').setup {
     icons = false, -- We have already setup dap icons so don't want them overridden here
     lsp_cfg = false, -- Use mason, then apply gopls config after setup
-    lsp_keymaps = false,
+    lsp_keymaps = function(bufnr)
+      local opts = function(values)
+        values.buffer = bufnr
+        return values
+      end
+      local keys = require("core.utils").keys
+      local prefix = '<leader>c' -- +Code
+      keys.nmap(prefix .. 'a', require('go.codeaction').run_code_action, opts { desc = "Go Code action" })
+      keys.vmap(prefix .. 'a', require('go.codeaction').run_range_code_action, opts { desc = "Go Code action" })
+      keys.nmap(prefix .. 'r', require('go.rename').lsprename, opts { desc = "Go Rename..."})
+    end,
     lsp_diag_virtual_text = false, -- Set up as part of diagnostics
     dap_debug_keymap = false, -- I have set this up already with all my other nvim-dap setup
     textobjects = false, -- Already have textobjects set up so do not add a mapping for this
