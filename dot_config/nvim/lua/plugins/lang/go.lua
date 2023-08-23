e5949566909b518a30b7a2ee86ce164a8e0c889a
Vincent Tencé <vtence@gmail.com>
2023-08-23 09:42:08 -0400

    feat(nvim): better gopls settings
    Configure `go` plugin and use its better configuration
    over the standard one. Disable builtin keymaps and icons
    to avoid overwriting are own.

--- dot_config/nvim/lua/plugins/lang/go.lua
+++ dot_config/nvim/lua/plugins/lang/go.lua
@@ -16,25 +16,33 @@ local M = {
 }
 
 function M.config()
-    -- TODO:
-    -- [] configure ginkgo binary
-    -- [] configure test coverage
-    -- [] configure symbols outline
-    -- [] configure lsp to use go.nvim cmp
-    -- [] use lsp snippets from go.nvim
-    -- [] move dap support to individual languages
-    -- [] move treesitter parsers inclusion to individual languages
-    -- [] configure treesitter textobjects
-    -- [] integrate with mason-lspconfig
-    -- [] integrate null-ls
-    -- [] add key mappings
+  -- TODO:
+  -- [] configure test coverage
+  -- [] configure symbols outline
+  -- [] use lsp snippets from go.nvim
+  -- [] integrate null-ls
+  -- [] add buffer key mappings for test, etc.
 
-    -- [] add https://github.com/jeniasaigak/goplay.nvim plugin
-    -- [] consider https://github.com/ray-x/navigator.lua plugin
+  -- [] add https://github.com/jeniasaigak/goplay.nvim plugin
+  -- [] consider https://github.com/ray-x/navigator.lua plugin
 
-    require('go').setup {
+  require('go').setup {
+    icons = false, -- We have already setup dap icons so don't want them overridden here
+    lsp_cfg = false, -- Use mason, then apply gopls config after setup
+    lsp_keymaps = false,
+    lsp_diag_virtual_text = false, -- Set up as part of diagnostics
+    dap_debug_keymap = false, -- I have set this up already with all my other nvim-dap setup
+    textobjects = false, -- Already have textobjects set up so do not add a mapping for this
+    run_in_floaterm = true,
+    -- TODO: make this a project specific setup, rather than a global setup?
+    test_runner = 'ginkgo',
+    trouble = true,
+    lua_snip = true,
+  }
 
-    }
+  -- Use go.nvim gopls config with lsp
+  local cfg = require('go.lsp').config()
+  require('lspconfig').gopls.setup(cfg)
 end
 
 return M
