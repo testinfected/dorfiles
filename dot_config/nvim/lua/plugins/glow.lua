bb0a68700b980bb186d79fada3e0a6af4e113c84
Vincent Tencé <vtence@gmail.com>
2023-08-25 14:41:17 -0400

    feat(nvim): add glow markdown previewer plugin

---
+++ dot_config/nvim/lua/plugins/glow.lua
@@ -0,0 +1,19 @@
+--[[
+  File: glow.lua
+  Description: Markdown preview with Glow
+  See: https://github.com/ellisonleao/glow.nvim
+]]
+
+local M = {
+  'ellisonleao/glow.nvim',
+  ft = { 'md' },
+  event = 'BufEnter'
+}
+
+function M.config()
+  return require('glow').setup {
+    border = 'rounded'
+  }
+end
+
+return M
