c7b459cfbcafab5ed78bc2fbbf5463a956a90dd7
Vincent Tencé <vtence@tower-research.com>
2023-09-19 13:32:47 -0400

    feat(nvim): null_ls go and autoformat

--- dot_config/nvim/lua/modules/lang/go.lua
+++ dot_config/nvim/lua/modules/lang/go.lua
@@ -42,6 +42,28 @@ function M.config()
   -- Use go.nvim gopls config with lsp
   local cfg = require('go.lsp').config()
   require('lspconfig').gopls.setup(cfg)
+
+  local null_ls = require("null-ls")
+  local sources = {
+      null_ls.builtins.diagnostics.revive,
+      null_ls.builtins.formatting.golines.with({
+          extra_args = {
+              "--max-len=180",
+              "--base-formatter=gofumpt",
+          },
+      })
+  }
+  -- for go.nvim
+  local gotest = require("go.null_ls").gotest()
+  local gotest_codeaction = require("go.null_ls").gotest_action()
+  local golangci_lint = require("go.null_ls").golangci_lint()
+  table.insert(sources, gotest)
+  table.insert(sources, golangci_lint)
+  table.insert(sources, gotest_codeaction)
+  null_ls.setup({ sources = sources, debounce = 1000, default_timeout = 5000 })
+
+  -- alternatively
+  --null_ls.register(gotest)
 end
 
 return M
